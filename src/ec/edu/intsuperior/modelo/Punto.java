3e012ab1d80c4be5097a35e7ee84cef9d37089f6
User <User@DESKTOP-9SNQ1MG>
2021-07-05 19:29:12 -0500

    Primer repositorio Github virtual

---
+++ src/ec/edu/intsuperior/modelo/Punto.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author User
+ */
+public class Punto {
+    private int x;
+    private int y;
+    
+    public Punto(){
+        
+    }
+    public Punto(int x, int y){
+        this.x= x;
+        this.y= y;
+          
+    }
+    public void setX(int x){
+        this.x=x;
+
+           }
+    public int getX(int x){
+        return x;
+    }
+    public void setY(int y){
+        this.y=y;
+    }
+    public int getY(int y){
+        return y;
+    }
+   public double distanciaEntreDosPuntos(int x1, int x2,int y1, int y2){
+       double d;
+      d=Math.sqrt((Math.pow(((double)(y2)-(double)(y2)),2)))+ (Math.pow(((double)(x2)-(double)(x1)),2));
+   
+}
+   
+
---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Punto;
+
+/**
+ *
+ * @author User
+ */
+public class Aplicacion {
+    public static void main (String[] args){
+        Punto p1=new Punto(5,7);
+        Punto p2=new Punto(0,0);
+        System.out.println("la distancia entre dos puntos son"+
+        p1.distanciaEntreDosPuntos(p1.getX(),p2.getX(),p1.getY(),p2.getY()));
+        
+    }
+            
+}
